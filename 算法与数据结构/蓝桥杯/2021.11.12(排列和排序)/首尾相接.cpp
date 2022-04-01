b806ae92f1eb10e602976504ec7251fb6651cbe3
Mieluoxxx <92699499+Mieluoxxx@users.noreply.github.com>
2022-04-01 23:39:39 +0800

    2022.4.1

--- 算法与数据结构/蓝桥杯/2021.11.12(排列和排序)/首尾相接.cpp
+++ 算法与数据结构/蓝桥杯/2021.11.12(排列和排序)/首尾相接.cpp
@@ -1,18 +1,23 @@
-// 用C++写正解
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
-string a[21];//以字符形式存储20个字符
 bool cmp(string a, string b)
 {
     return a + b >  b + a;
 }
 int main()
 {
-    int n;  cin >> n;
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
-    sort(a, a+n, cmp);
-    for(int i = 0; i < n; i++)
-        cout << a[i];
+    int n;  
+    while(cin >> n)
+    {
+        string a[21];
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+        sort(a, a+n, cmp);
+        for(int i = 0; i < n; i++)
+            cout << a[i];
+        cout << endl;
+    }
     return 0;
 }
